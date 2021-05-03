6fa01f1dff78cc633147ff0faeaefe591f60670b
ElisabetJenni <74007520+ElisabetJenni@users.noreply.github.com>
2021-05-04 00:38:48 +0700

    Add files via upload

---
+++ M03-npm/app.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var server = express();
+var logger = require("morgan");
+
+server.use(logger("dev"));
+
+server.use(express.static(__dirname + "/publik"));
+
+server.listen(3400, function () {
+  console.log("Server run");
+});
